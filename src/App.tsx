95edd87f75e1a6e302258eb4acb9b207de9181b9
Terratical <lakonimangil@gmail.com>
2026-04-03 21:36:51 +0000

    social links

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import Avatar from "../assets/images/avatar-jessica.jpeg";
+
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <article className="min-w-[min(90%,20rem)] rounded-xl bg-(--bg-card) p-(--spacing-xl) text-center sm:min-w-[min(90%,25rem)] sm:p-(--spacing-2xl)">
+        <figure className="flex flex-col items-center">
+          <img className="w-24 rounded-full" src={Avatar} alt="Jessica Randall Profile Photo" />
+          <figcaption className="mt-(--spacing-xl)">
+            <h1 className="text-full-name">Jessica Randall</h1>
+            <p className="text-location">London, United Kingdom</p>
+          </figcaption>
+        </figure>
+        <p className="mt-(--spacing-xl) text-base text-(--text-white)">
+          "Front-end developer and avid reader."
+        </p>
+        <ul
+          id="social-links"
+          className="mt-(--spacing-xl) flex flex-col gap-(--spacing-lg)"
+        >
+          <li>
+            <a
+              className="block rounded-lg bg-(--bg-btn) p-(--spacing-md) text-base font-bold"
+              href="https://github.com"
+            >
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a
+              className="block rounded-lg bg-(--bg-btn) p-(--spacing-md) text-base font-bold"
+              href="https://frontendmentor.io"
+            >
+              Frontend Mentor
+            </a>
+          </li>
+          <li>
+            <a
+              className="block rounded-lg bg-(--bg-btn) p-(--spacing-md) text-base font-bold"
+              href="https://linkedin.com"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              className="block rounded-lg bg-(--bg-btn) p-(--spacing-md) text-base font-bold"
+              href="https://twitter.com"
+            >
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a
+              className="block rounded-lg bg-(--bg-btn) p-(--spacing-md) text-base font-bold"
+              href="https://instagram.com"
+            >
+              Instagram
+            </a>
+          </li>
+        </ul>
+      </article>
+    </main>
+  );
+}
+
+export default App;
